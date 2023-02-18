491fbefff1c7667a8092f527cccbc6c369c3e774
Cormac McGaughey <cormac.mcgaughey@gmail.com>
2023-02-17 22:31:34 -0800

    forgot to add the TODO // #add a todo file.

---
+++ todos.pas
@@ -0,0 +1,7 @@
+{ #todo 1: Figure out zip files - look for .dsk, etc inside to validate
+ }
+{ #todo 3 -cfutureplans: could I add support for Spectrum, C64, dosbox?
+ }
+{ #todo 2: Figure out how to process file.diz files for more information }
+
+
